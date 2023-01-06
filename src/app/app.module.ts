c83e220694a813a63e05d51c99635938486bf4fb
gisotto <gi@sotto.com.br>
2023-01-06 13:22:53 -0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiroComponent2Component } from './meu-primeiro-component2/meu-primeiro-component2.component';
-import { MeuPrimeiroComponent3Component } from './meu-primeiro-component3/meu-primeiro-component3.component';
-import { MeuPrimeiroCompoment4Component } from './meu-primeiro-compoment4/meu-primeiro-compoment4.component';
-import { MeuQuintoComponent } from './meu-quinto/meu-quinto.component';
+
 
 
 @NgModule({
@@ -15,9 +13,7 @@ import { MeuQuintoComponent } from './meu-quinto/meu-quinto.component';
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiroComponent2Component,
-    MeuPrimeiroComponent3Component,
-    MeuPrimeiroCompoment4Component,
-    MeuQuintoComponent,
+    
   ],
 
   imports: [
